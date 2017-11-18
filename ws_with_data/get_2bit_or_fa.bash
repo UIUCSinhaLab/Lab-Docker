52a89bac0cee03e6c10c11036f537332ebe88d4f
Bryan Lunt <bjlunt2@illinois.edu>
2017-11-17 22:49:24 -0600

    added script

---
+++ ws_with_data/get_2bit_or_fa.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+NAME=$1
+
+BASE_DIR="ftp://hgdownload.soe.ucsc.edu/goldenPath/"
+
+TARGET_DIR="${BASE_DIR}/${NAME}/bigZips/"
+
+tmp_filename=$(mktemp)
+
+curl ${TARGET_DIR} | awk '$9~/.2bit/' > ${tmp_filename}
+
+if [[ -s ${tmp_filename} ]]
+then
+	#get the .2bit file
+	echo "GETTING 2bit"
+
+	wget "${TARGET_DIR}"'/*.2bit'
+
+else
+	echo "GETTING fasta and _creating_ 2bit"
+
+	wget "${TARGET_DIR}/${NAME}/bigZips/${NAME}.fa.gz"
+	gunzip "${NAME}.fa.gz"
+	faToTwoBit "${NAME}.fa" ${NAME}.2bit
+	rm "${NAME}.fa"
+fi
