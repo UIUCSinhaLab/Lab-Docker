7fc2115e66c75404f3d7eb369cd6d6548280a803
Bryan Lunt <bjlunt2@illinois.edu>
2018-05-27 13:45:11 -0500

    Reworked notebooks to be more modular, include more features.

--- build.bash
+++ build.bash
@@ -1,5 +1,8 @@
+#Basic Tools
 docker build -t miniconda3 miniconda3/
 docker build -t bioconda3 bioconda3/
-docker build -t genome_workspace genome_workspace/
-docker build -t ws_with_data ws_with_data/
 docker build -t jpnb jupyter_nb/
+docker build -t jplab jupyter_lab/
+
+#docker build -t genome_workspace genome_workspace/
+#docker build -t ws_with_data ws_with_data/
